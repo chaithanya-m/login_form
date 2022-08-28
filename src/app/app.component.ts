e394a648a450279c4203a2f422894cbb12d9b6b5
chaithanya <chaithanyamadduru03@gmail.com>
2022-08-28 11:12:37 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ColdObservable } from 'rxjs/internal/testing/ColdObservable';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'login_from';
+  title = 'login_form';
+  verifyname(){
+   
+    let n: string = document.getElementById("name")!.innerHTML;
+  
+   if(n.length < 5){
+     document.getElementById("name")!.style.color ="red";
+   }
+   else{
+    document.getElementById("name")!.style.color ="black";
+
+   }
+
+  }
+  verifyemail(){
+    alert("emai");
+
+  }
+  verifypassword(){
+    alert("password");
+
+  }
+  submitbutton(){
+    alert("submit");
+
+  }
+
 }
